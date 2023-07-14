741bb4fb39fd6e307717f3218bb4dee7575810ce
milahnmartin <milahnmartin.develop@gmail.com>
2023-07-14 22:24:38 +0200

    init firebase

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"firebase-admin-service/controller"
+	"context"
 	"fmt"
+
+	firebase "firebase.google.com/go"
+
+	"google.golang.org/api/option"
 )
 
 func main() {
-	fmt.Print("Hello World")
-	controller := controller.NewUserController()
+	opt := option.WithCredentialsFile("../itbsa-honours-backend-firebase-adminsdk-taci1-f6ac4b2b90.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(app)
 }
--- models/index.go
+++ models/index.go
@@ -1,16 +1,1 @@
 package models
-
-// User represents a user entity
-type User struct {
-    ID   int
-    Name string
-    // Add more fields as needed
-}
-
-// Post represents a post entity
-type Post struct {
-    ID     int
-    Title  string
-    Author string
-    // Add more fields as needed
-}
